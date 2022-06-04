3e07f342a4d57839ddc1b6408b339e1d6cdd8fff
Marko Nalis <marko@nalis.dev>
2022-06-04 14:30:55 +0200

    * added sample for async request

--- README.md
+++ README.md
@@ -56,6 +56,47 @@ You can set it as environment variable or whatever makes sense for your Applicat
         channel.shutdownNow();
 ```
 
+For Async Futures you can use this:
+
+```
+        String API_KEY = System.getenv("API_KEY");
+        var channel = ManagedChannelBuilder.forAddress("speech.citizenjournalist.io", 443)
+                .build();
+        var callCredentials = new BearerToken(API_KEY);
+        var stub = SubtitlingGrpc.newStub(channel).withCallCredentials(callCredentials);
+
+        StreamObserver<TranscriptionReply> responseObserver = new StreamObserver<TranscriptionReply>() {
+            @Override
+            public void onNext(TranscriptionReply next) {
+            System.out.println("---");
+            System.out.println(next);
+            System.out.println("---");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println("An error happened " + t.getMessage());
+                countdownLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("done");
+                countdownLatch.countDown();
+            }
+        };
+
+        stub.transcribe(TranscriptionRequest
+                        .newBuilder()
+                        .setExternalReference("abcd")
+                        .setSourceUrl("https://ipfs.citizenjournalist.io/ipfs/QmPuqoid7n12tR7LkyX6db7hiYWSvXBYTnYejn4rZDJqsY")
+                        .addAllOutputLanguages(List.of(Language.EN, Language.DE, Language.FR, Language.IT, Language.ES, Language.CA))
+                        .addAllFormat(List.of(SubtitleFormat.SRT, SubtitleFormat.VTT, SubtitleFormat.MP3))
+                        .build(), responseObserver
+        ); // IMPORTANT this assumes that your application is running as a server or similar. If you run this code by itself it will terminate immediately as the operation is executed asyncronously
+        // channel.shutdownNow(); // add this in your application after your request,session or similar to cleanup the channel
+```
+
 Take a look in GrpcClientTest for the sample code.
 
 Further details about the fields, their meaning and usage can be found here:
--- src/test/java/GrpcClientTest.java
+++ src/test/java/GrpcClientTest.java
@@ -1,10 +1,13 @@
 import ch.swisstxt.citizenjournalist.sftpprocessor.grpc.*;
 import ch.swisstxt.citizenjournalist.sftpprocessor.grpc.Language;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 class GrpcClientTest {
 
@@ -30,6 +33,56 @@ class GrpcClientTest {
             System.out.println(next);
             System.out.println("---");
         }
+
+        // A Channel should be shutdown before stopping the process.
+        channel.shutdownNow();
+    }
+
+    @Test
+    void testGrpcServerAsync() throws InterruptedException {
+        String API_KEY = System.getenv("API_KEY");
+        var channel = ManagedChannelBuilder.forAddress("speech.citizenjournalist.io", 443)
+                .build();
+        var callCredentials = new BearerToken(API_KEY);
+        var stub = SubtitlingGrpc.newStub(channel).withCallCredentials(callCredentials);
+
+        // this is just to keep the test running until we get the results,
+        // usually the application would keep running on main thread by itself
+        var countdownLatch = new CountDownLatch(1);
+
+        StreamObserver<TranscriptionReply> responseObserver = new StreamObserver<>() {
+            @Override
+            public void onNext(TranscriptionReply next) {
+                System.out.println("---");
+                System.out.println(next);
+                System.out.println("---");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                System.err.println("An error happened " + t.getMessage());
+                countdownLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("done");
+                countdownLatch.countDown();
+            }
+        };
+
+        stub.transcribe(TranscriptionRequest
+                        .newBuilder()
+                        .setExternalReference("abcd")
+                        .setSourceUrl("https://ipfs.citizenjournalist.io/ipfs/QmPuqoid7n12tR7LkyX6db7hiYWSvXBYTnYejn4rZDJqsY")
+                        .addAllOutputLanguages(List.of(Language.EN, Language.DE, Language.FR, Language.IT, Language.ES, Language.CA))
+                        .addAllFormat(List.of(SubtitleFormat.SRT, SubtitleFormat.VTT))
+                        .build(), responseObserver
+        );
+
+        // this is not necessary when running on a server but we need to keep the test runnung until results arrive.
+        // on running application your server would ensure to keep running
+       countdownLatch.await(10, TimeUnit.MINUTES); // set timeout according to needs for the sample we set it to 10 minutes, if you have large videos it should be higher
 
         // A Channel should be shutdown before stopping the process.
         channel.shutdownNow();
